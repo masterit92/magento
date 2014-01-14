3bcfc6a1bb745ca7391641128064051f8de1c31a
masterit92 <thebinh1992@gmail.com>
2014-01-14 08:47:52 +0700

    start

---
+++ app/code/local/SM/MyCart/sql/sm_mycart_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: smartosc
+ * Date: 1/13/14
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
